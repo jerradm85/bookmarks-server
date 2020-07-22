7bc953ee5faef1808dc579d0b8f608272ccaeeb9
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-07-21 23:31:03 -0700

    add malicious bookmark entries in fixtures file for use in testing

--- test/bookmarks.fixtures.js
+++ test/bookmarks.fixtures.js
@@ -31,20 +31,20 @@ function makeBookmarksArray() {
     ]
 }
 
-function maliciousBookmarkItems() {
+function maliciousBookmarks() {
   const maliciousBookmark = {
     id: 911,
     title: 'Naughty naughty very naughty <script>alert("xss");</script>',
-    url: 'http://example.com',
-    content: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`
-  }
-  
+    url: 'http://example.com <script>alert("xss");</script>',
+    description: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`,
+  };
+
   const expectedBookmark = {
     id: 911,
-    title: 'Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;',
-    url: 'http://example.com',
-    content: `Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`
-  }
+    title: 'Naughty naughty very naughty &lt;script&gt;alert("xss");&lt;/script&gt;',
+    url: 'http://example.com &lt;script&gt;alert("xss");&lt;/script&gt;',
+    description: `Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`,
+  };
 
   return {
     maliciousBookmark,
@@ -55,6 +55,5 @@ function maliciousBookmarkItems() {
 
 module.exports = {
     makeBookmarksArray,
-    maliciousBookmarkItems
-
+    maliciousBookmarks
 }
